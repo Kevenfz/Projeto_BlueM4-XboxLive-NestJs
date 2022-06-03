77ba41f77a5aad93396ce985f3d96db8f7ca562a
Keven Ferreira Moraes <55964670+Kevenfz@users.noreply.github.com>
2022-06-03 16:55:24 -0300

    add: Criação da baseUrl e configuração do trust proxy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 @ApiTags('status')
@@ -9,7 +10,8 @@ export class AppController {
 
   @Get()
   @ApiOperation({ summary: 'Visualizar status da aplicação' })
-  getAppStatus(): string {
-    return this.appService.getAppStatus();
+  getAppStatus(@Req() req: Request) {
+    const baseUrl = req.protocol + '://' + req.get('host');
+    return this.appService.getAppStatus(baseUrl);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  app.set('trust proxy', 1);
 
   app.useGlobalPipes(new ValidationPipe());
 
